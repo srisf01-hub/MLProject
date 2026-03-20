5cb99685430a95b436553e7c3c506c773ab6e14d
Srilakshmi <srisf01@gmail.com>
2026-03-20 15:12:35 -0400

    Update data pipeline and add preprocessor artifact

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -38,7 +38,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -51,7 +51,7 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
-    print(train_data, test_data)
+    #print(train_data, test_data)
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
